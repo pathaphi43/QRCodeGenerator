b0759eb38a4f5cccb325867f405e67fc1b9b4852
pathaphi.p <pathaphi.p@takeitsolution.com>
2023-10-16 11:35:12 +0700

    Fix QR Code accept UTF-8

--- src/main/java/org/example/GenerateQRCode.java
+++ src/main/java/org/example/GenerateQRCode.java
@@ -6,59 +6,21 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Map;
 
 public class GenerateQRCode {
 
     // Function to create the QR code
     public void createQR(String data, String path, int height, int width) throws WriterException, IOException {
-
-        // Encoding charset
-        String charset = "UTF-8";
-
-        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, width, height);
-
+        Map<EncodeHintType, Charset> hashMap = new HashMap<>();
+        hashMap.put(EncodeHintType.CHARACTER_SET, StandardCharsets.UTF_8);
+        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, width, height, hashMap);
         MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
-    }
-
-    public void createQRImage(File qrFile, String qrCodeText, int size, String fileType) throws WriterException, IOException {
-        // Create the ByteMatrix for the QR-Code that encodes the given String
-//        Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
-        Hashtable<EncodeHintType, String> hintMap = new Hashtable<>();
-//        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-        hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
-        // Make the BufferedImage that are to hold the QRCode
-        int matrixWidth = byteMatrix.getWidth();
-        BufferedImage image = new BufferedImage(matrixWidth, matrixWidth, BufferedImage.TYPE_INT_RGB);
-        image.createGraphics();
-
-        Graphics2D graphics = (Graphics2D) image.getGraphics();
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, matrixWidth, matrixWidth);
-        // Paint and save the image using the ByteMatrix
-        graphics.setColor(Color.BLACK);
-
-        for (int i = 0; i < matrixWidth; i++) {
-            for (int j = 0; j < matrixWidth; j++) {
-                if (byteMatrix.get(i, j)) {
-                    graphics.fillRect(i, j, 1, 1);
-                }
-            }
-        }
-        ImageIO.write(image, fileType, qrFile);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,20 @@
 package org.example;
 
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         GenerateQRCode generateQRCode = new GenerateQRCode();
-        Scanner scan= new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter message:");
-        String qrCodeText= scan.nextLine();
+        String qrCodeText = scan.nextLine();
         String filePath = "QRCode.png";
         int size = 200;
-        String fileType = "png";
-        File qrFile = new File(filePath);
         try {
+            System.out.println(qrCodeText);
             generateQRCode.createQR(qrCodeText, filePath, size, size);
-        }catch (Exception ex){
-            System.out.println("Error"+ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Error" + ex.getMessage());
         }
         System.out.println("DONE");
     }
